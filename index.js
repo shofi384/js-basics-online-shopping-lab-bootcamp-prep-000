22676f88473f4aa337a51f48ed6115ff961eab2c
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-11 14:36:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ function placeOrder(cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
   else {
+    var t = total()
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`
   }
 }
