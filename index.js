dc32057bcec30ed5240a54761cef3990b282ff60
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-11 14:31:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,5 +64,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if(cardNumber === undefined) {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
+  else {
+    
+  }
 }
